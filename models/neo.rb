1d0282c56a2d07934077fe81870f87eda05a72ba
Tech-A04 <tech-a04@tech-a04s-air.home>
2017-12-26 19:48:14 +0000

    connection done with neonasa database, all CRUD routing is working

--- models/neo.rb
+++ models/neo.rb
@@ -1,26 +1,25 @@
-class Neo
+class Post
 
   attr_accessor :id, :title, :body
-
   def save
     conn = Post.open_connection
     if(self.id)
-      sql = "UPDATE post SET title='#{self.title}', body='#{self.body}' WHERE id = #{self.id}"
+      sql = "UPDATE neo SET title='#{self.title}', body='#{self.body}' WHERE id = #{self.id}"
     else
-      sql = "INSERT INTO post (title, body) VALUES ('#{self.title}', '#{self.body}')"
+      sql = "INSERT INTO neo (title, body) VALUES ('#{self.title}', '#{self.body}')"
     end
     conn.exec(sql)
   end
 
   def self.api_create(post)
     conn = self.open_connection
-    sql = "INSERT INTO post (title , body) VALUES ( '#{post[:title]}', '#{post[:body]}')"
+    sql = "INSERT INTO neo (title , body) VALUES ( '#{post[:title]}', '#{post[:body]}')"
     conn.exec(sql)
   end
 
   def self.api_update(post)
     conn = self.open_connection
-    sql = "UPDATE post SET title='#{post[:title]}', body='#{post[:body]}' WHERE id = #{post[:id]}"
+    sql = "UPDATE neo SET title='#{post[:title]}', body='#{post[:body]}' WHERE id = #{post[:id]}"
     conn.exec(sql)
   end
 
@@ -30,7 +29,7 @@ class Neo
 
   def self.all
     conn = self.open_connection
-    sql = "SELECT id,title,body FROM post ORDER BY id DESC"
+    sql = "SELECT id,title,body FROM neo ORDER BY id DESC"
     results = conn.exec(sql)
     posts = results.map do |post|
       self.hydrate(post)
@@ -39,21 +38,21 @@ class Neo
 
   def self.find(id)
     conn = self.open_connection
-    sql = "SELECT * FROM post WHERE id =#{id} LIMIT 1"
+    sql = "SELECT * FROM neo WHERE id =#{id} LIMIT 1"
     posts = conn.exec(sql)
     self.hydrate(posts[0])
   end
 
   def self.apiFind(id)
     conn = self.open_connection
-    sql = "SELECT * FROM post WHERE id =#{id} LIMIT 1"
+    sql = "SELECT * FROM neo WHERE id =#{id} LIMIT 1"
     posts = conn.exec(sql)
     posts[0]
   end
 
   def self.destroy(id)
     conn = self.open_connection
-    sql = "DELETE FROM post WHERE id = #{id}"
+    sql = "DELETE FROM neo WHERE id = #{id}"
     conn.exec(sql)
   end
 

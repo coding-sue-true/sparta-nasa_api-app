d5ac22a3d23b7e4faf1b5ce8be0b19312fda2a85
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-01-02 11:52:48 +0000

    some tests added

--- testing/lib/services/neo_feed.rb
+++ testing/lib/services/neo_feed.rb
@@ -15,12 +15,36 @@ class NeoFeedService
     @neo_feed_data = JSON.parse(self.class.get("/feed?start_date=#{start_date}&end_date=#{end_date}&api_key=#{api_key}").body)
   end
 
+  def get_neo_feed_all
+    @neo_feed_data
+  end
+
   def get_neo_feed_links
     @neo_feed_data['links']
   end
 
+  def get_next
+    get_neo_feed_links['next']
+  end
+
+  def get_prev
+    get_neo_feed_links['prev']
+  end
+
+  def get_self
+    get_neo_feed_links['self']
+  end
+
   def get_neo_feed_elementcount
     @neo_feed_data['element_count']
+  end
+
+  def get_neo_feed_neo
+    @neo_feed_data['near_earth_objects']
+  end
+
+  def get_date
+    get_neo_feed_neo.first
   end
 end
 
--- testing/spec/neo_feed_spec.rb
+++ testing/spec/neo_feed_spec.rb
@@ -9,12 +9,36 @@ describe Neoapi do
       @neoapi.get_neo_feed_results('2017-12-22', '2017-12-25')
     end
 
-    it "Should respond with a status message of 200" do
+    it "Should respond with an Hash of Results" do
+      expect(@neoapi.get_neo_feed_all).to be_kind_of(Hash)
+    end
+
+    it "Links should respond with an Hash of Results" do
       expect(@neoapi.get_neo_feed_links).to be_kind_of(Hash)
     end
 
-    it "Should respond with a status message of 200" do
+    it "Next should respond as a String" do
+      expect(@neoapi.get_next).to be_kind_of(String)
+    end
+
+    it "Prev should respond as a String" do
+      expect(@neoapi.get_prev).to be_kind_of(String)
+    end
+
+    it "Self should respond as a String" do
+      expect(@neoapi.get_self).to be_kind_of(String)
+    end
+
+    it "Element Count should respond with an Integer" do
       expect(@neoapi.get_neo_feed_elementcount).to be_kind_of(Integer)
+    end
+
+    it "Near Earth Objects should respond as a Hash" do
+      expect(@neoapi.get_neo_feed_neo).to be_kind_of(Hash)
+    end
+
+    it "Near Earth Objects date should respond as a String" do
+      expect(@neoapi.get_date).to be_kind_of(Hash)
     end
 
   end

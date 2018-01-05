2a717140d7071958aefc24847d3c3a5688b5f18d
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-01-05 20:11:47 +0000

    code revised

--- testing/ui_testing/features/support/env.rb
+++ testing/ui_testing/features/support/env.rb
@@ -15,7 +15,6 @@ Capybara.configure do |config|
   config.default_max_wait_time= 10 #wait time for asynchronus processes to finsh
   config.match = :prefer_exact #this setting is to ensure Capybara has specific matching rather than fuzzy logic
   config.default_driver = :chrome # ensures chrome is the default driver
-  # config.app_host = 'localhost:9292/' # provides the app host/core url
 end
 
 World(NasaWebApp)

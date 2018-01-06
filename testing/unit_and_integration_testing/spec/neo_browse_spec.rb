775e3d95b33b8ba273105868e1ea58f74a3334ba
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-01-06 01:20:30 +0000

    browse api testing now looping through all the data

--- testing/unit_and_integration_testing/spec/neo_browse_spec.rb
+++ testing/unit_and_integration_testing/spec/neo_browse_spec.rb
@@ -54,167 +54,249 @@ describe Neoapi do
     end
 
     it "NEO links should respond as a Hash of results" do
-      expect(@neoapi.get_links).to be_kind_of(Hash)
+      @neoapi.get_links.each do |x|
+        expect(x).to be_kind_of(Hash)
+      end
     end
 
     it "Near Earth Objects links self should respond as a String" do
-      expect(@neoapi.get_links_self).to be_kind_of(String)
+      @neoapi.get_links_self.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects neo_reference_id to respond as a String" do
-      expect(@neoapi.get_neo_reference_id).to be_kind_of(String)
+      @neoapi.get_neo_reference_id.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects neo name to respond as a String" do
-      expect(@neoapi.get_neo_name).to be_kind_of(String)
+      @neoapi.get_neo_name.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects neo jpl_url to respond as a String" do
-      expect(@neoapi.get_neo_nasa_jpl_url).to be_kind_of(String)
+      @neoapi.get_neo_nasa_jpl_url.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects neo absolute magnitude to respond as Float" do
-      expect(@neoapi.get_neo_absolute_magnitude_h).to be_kind_of(Float)
+      @neoapi.get_neo_absolute_magnitude_h.each do |x|
+        expect(x).to be_kind_of(Float)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter to respond as a Hash" do
-      expect(@neoapi.get_neo_estimated_diameter).to be_kind_of(Hash)
+      @neoapi.get_neo_estimated_diameter.each do |x|
+        expect(x).to be_kind_of(Hash)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter kilometers to respond as a Hash" do
-      expect(@neoapi.get_km).to be_kind_of(Hash)
+      @neoapi.get_km.each do |x|
+        expect(x).to be_kind_of(Hash)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter km min to respond as Float" do
-      expect(@neoapi.get_km_estimated_diameter_min).to be_kind_of(Float)
+      @neoapi.get_km_estimated_diameter_min.each do |x|
+        expect(x).to be_kind_of(Float)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter km max to respond as Float" do
-      expect(@neoapi.get_km_estimated_diameter_max).to be_kind_of(Float)
+      @neoapi.get_km_estimated_diameter_max.each do |x|
+        expect(x).to be_kind_of(Float)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter meters to respond as a Hash" do
-      expect(@neoapi.get_meters).to be_kind_of(Hash)
+      @neoapi.get_meters.each do |x|
+        expect(x).to be_kind_of(Hash)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter meters min to respond as a Float" do
-      expect(@neoapi.get_meters_estimated_diameter_min).to be_kind_of(Float)
+      @neoapi.get_meters_estimated_diameter_min.each do |x|
+        expect(x).to be_kind_of(Float)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter meters max to respond as a Float" do
-      expect(@neoapi.get_meters_estimated_diameter_max).to be_kind_of(Float)
+      @neoapi.get_meters_estimated_diameter_max.each do |x|
+        expect(x).to be_kind_of(Float)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter miles to respond as a Hash" do
-      expect(@neoapi.get_miles).to be_kind_of(Hash)
+      @neoapi.get_miles.each do |x|
+        expect(x).to be_kind_of(Hash)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter miles min to respond as a Float" do
-      expect(@neoapi.get_miles_estimated_diameter_min).to be_kind_of(Float)
+      @neoapi.get_miles_estimated_diameter_min.each do |x|
+        expect(x).to be_kind_of(Float)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter miles max to respond as a Float" do
-      expect(@neoapi.get_miles_estimated_diameter_max).to be_kind_of(Float)
+      @neoapi.get_miles_estimated_diameter_max.each do |x|
+        expect(x).to be_kind_of(Float)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter feet to respond as a Hash" do
-      expect(@neoapi.get_feet).to be_kind_of(Hash)
+      @neoapi.get_feet.each do |x|
+        expect(x).to be_kind_of(Hash)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter feet min to respond as a Float" do
-      expect(@neoapi.get_feet_estimated_diameter_min).to be_kind_of(Float)
+      @neoapi.get_feet_estimated_diameter_min.each do |x|
+        expect(x).to be_kind_of(Float)
+      end
     end
 
     it "Near Earth Objects neo estimated diameter feet max to respond as a Float" do
-      expect(@neoapi.get_feet_estimated_diameter_max).to be_kind_of(Float)
+      @neoapi.get_feet_estimated_diameter_max.each do |x|
+        expect(x).to be_kind_of(Float)
+      end
     end
 
     it "Near Earth Objects is potentially hazardous to respond as a boolean" do
-      expect(@neoapi.get_potentially_hazardous).to be_truthy.or be_falsy
+      @neoapi.get_potentially_hazardous.each do |x|
+        expect(x).to be_truthy.or be_falsy
+      end
     end
 
     it "Near Earth Objects close approach data to respond as a Array" do
-      expect(@neoapi.get_close_approach_data).to be_kind_of(Array)
+      @neoapi.get_close_approach_data.each do |x|
+        expect(x).to be_kind_of(Array)
+      end
     end
 
     it "Near Earth Objects orbital data to respond as a Hash of results" do
-      expect(@neoapi.get_orbital_data).to be_kind_of(Hash)
+      @neoapi.get_orbital_data.each do |x|
+        expect(x).to be_kind_of(Hash)
+      end
     end
 
     it "Near Earth Objects orbital id to respond as a String" do
-      expect(@neoapi.get_orbit_id).to be_kind_of(String)
+      @neoapi.get_orbit_id.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects orbital determination date to respond as a String" do
-      expect(@neoapi.get_orbit_determin_date).to be_kind_of(String)
+      @neoapi.get_orbit_determin_date.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects orbital uncertainty to respond as a String" do
-      expect(@neoapi.get_orbit_uncertainty).to be_kind_of(String)
+      @neoapi.get_orbit_uncertainty.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects orbit minimun intersection to respond as a String" do
-      expect(@neoapi.get_orbit_min_intersec).to be_kind_of(String)
+      @neoapi.get_orbit_min_intersec.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects jupiter tisserand invariant to respond as a String" do
-      expect(@neoapi.get_jupiter_tisserand_inv).to be_kind_of(String)
+      @neoapi.get_jupiter_tisserand_inv.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects epoch_osculation to respond as a String" do
-      expect(@neoapi.get_epoch).to be_kind_of(String)
+      @neoapi.get_epoch.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects eccentricity to respond as a String" do
-      expect(@neoapi.get_eccentricity).to be_kind_of(String)
+      @neoapi.get_eccentricity.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects semi_major_axis to respond as a String" do
-      expect(@neoapi.get_semi_major_axis).to be_kind_of(String)
+      @neoapi.get_semi_major_axis.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects inclination to respond as a String" do
-      expect(@neoapi.get_inclination).to be_kind_of(String)
+      @neoapi.get_inclination.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects ascending_node_longitude to respond as a String" do
-      expect(@neoapi.get_ascending_node_long).to be_kind_of(String)
+      @neoapi.get_ascending_node_long.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects orbital period to respond as a String" do
-      expect(@neoapi.get_orbital_period).to be_kind_of(String)
+      @neoapi.get_orbital_period.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects perihelion_distance to respond as a String" do
-      expect(@neoapi.get_perihelion_distance).to be_kind_of(String)
+      @neoapi.get_perihelion_distance.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects perihelion_argument to respond as a String" do
-      expect(@neoapi.get_perihelion_argument).to be_kind_of(String)
+      @neoapi.get_perihelion_argument.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects aphelion_distance to respond as a String" do
-      expect(@neoapi.get_aphelion_distance).to be_kind_of(String)
+      @neoapi.get_aphelion_distance.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects perihelion_time to respond as a String" do
-      expect(@neoapi.get_perihelion_time).to be_kind_of(String)
+      @neoapi.get_perihelion_time.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects mean_anomaly to respond as a String" do
-      expect(@neoapi.get_mean_anomaly).to be_kind_of(String)
+      @neoapi.get_mean_anomaly.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects mean_motion to respond as a String" do
-      expect(@neoapi.get_mean_motion).to be_kind_of(String)
+      @neoapi.get_mean_motion.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects equinox to respond as a String" do
-      expect(@neoapi.get_equinox).to be_kind_of(String)
+      @neoapi.get_equinox.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
 
     it "Near Earth Objects orbital id to respond as a String" do
-      expect(@neoapi.get_orbit_id).to be_kind_of(String)
+      @neoapi.get_orbit_id.each do |x|
+        expect(x).to be_kind_of(String)
+      end
     end
   end
 end
